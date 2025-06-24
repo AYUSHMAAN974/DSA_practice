ee1e621640497f62ffc90656b411b7a200130600
Ayushmaan Gupta <134031004+AYUSHMAAN974@users.noreply.github.com>
2025-06-24 21:21:14 +0530

    Update Longest_substring_with_sum_k.py

--- Longest_substring_with_sum_k.py
+++ Longest_substring_with_sum_k.py
@@ -1,19 +1,34 @@
-def Longest_Substring(N, array, s):
-    lp = 0
-    max_len = 0
+def Longest_Subarray_With_Sum_K(array, k):
+    prefix_map = {}    
     curr_sum = 0
-    
-    for rp in range(N):
-        curr_sum += array[rp]
-        
-        while curr_sum > s and lp <= rp:
-            curr_sum -= array[lp]
-            lp += 1
-        
-        if curr_sum == s:
-            length = rp - lp + 1
-            max_len = max(max_len, length)
-            
+    max_len = 0
+    start_index = -1    
+
+    for i in range(len(array)):
+        curr_sum += array[i]
+
+        if curr_sum == k:
+            max_len = i + 1
+            start_index = 0
+
+        if (curr_sum - k) in prefix_map:
+            prev_index = prefix_map[curr_sum - k]
+            length = i - prev_index
+            if length > max_len:
+                max_len = length
+                start_index = prev_index + 1
+
+        if curr_sum not in prefix_map:
+            prefix_map[curr_sum] = i
+
+    if max_len == 0:
+        print("No subarray found with sum =", k)
+    else:
+        print("Longest subarray with sum", k, "is:")
+        print(array[start_index : start_index + max_len])
+        print("Length:", max_len)
+
     return max_len
 
-print(Longest_Substring(7, [1, 2, 3, 1, 1, 1, 1], 3))  
+
+Longest_Subarray_With_Sum_K([1, -1, 5, -2, 1, 1, -2, 4], 3)
